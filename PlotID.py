26f4b101a58d278e9453a8a9b08ddef6b09789b3
karakoulaki <k.karakoulaki23@imperial.ac.uk>
2024-02-07 14:58:38 +0000

    added modules.py to change number of modules

--- PlotID.py
+++ PlotID.py
@@ -2,23 +2,24 @@ import DetectorGenerator
 import PatternEncoder
 import DetectorTrackGraphMatplotlib
 import matplotlib as mpl
+import modules
 mpl.use('Agg')
 
 from math import *
 import matplotlib.pyplot as plt
 import pickle
 import sys
-
+modules=modules.ModuleNumber()
 # Generate detector again TODO save the detector from the track generator file
 # Initialise the detector
 detector = DetectorGenerator.DetectorGenerator()
 # How many layers in the detector
-detector.NumberOfLayers  = 4  
+detector.NumberOfLayers  = modules.changedetector()[2] 
 # How many modules in each layer 
-detector.NumberOfModules = [3,3,3,3]
+detector.NumberOfModules = modules.changedetector()[0]
 # How long is each module in each layer of the detector, no overlaps so module length < 2/3 where 3 is from number of modules 
 # in each layer and 2 from the fact the detector spans -1 to 1
-detector.ModuleLength    = [0.63,0.63,0.63,0.63]
+detector.ModuleLength    = modules.changedetector()[1]
 # How far from the origin is each layer
 detector.RadialPosition  = [1.0,1.5,2.0,2.5]
 detector.Generate()
