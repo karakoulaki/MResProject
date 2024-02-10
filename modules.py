1aec0e9f697ba345a0e7b7706d98b1319b4e894d
karakoulaki <k.karakoulaki23@imperial.ac.uk>
2024-02-10 14:36:50 +0000

    changing phi and curvature

--- modules.py
+++ modules.py
@@ -1,12 +1,19 @@
+import numpy as np
+
 class ModuleNumber:
 
     def __init__(self):
         self.nlayers = 4 #number of layers 
-        self.n = 2 #number to multiply the inital number of modules in each layer, and the length of each module is going to be divided with it
+        self.n = 1 #number to multiply the inital number of modules in each layer, and the length of each module is going to be divided with it
         self.distradialposition = 0.5 # distance between the layers 
         self.firstposition = 1.0 #radial position of the first layer
-    
+        self.phi0_range = [np.pi,np.pi]
+        self.curvature_range = [19,18]
+        
     def changedetector(self):
+        #modules=[3,3,3,3]
+        #length=[0.63,0.63,0.63,0.63]
+        #radialposition=[1.0,1.5,2.0,2.5]
         modules=[]
         length=[]
         radialposition=[]
@@ -17,5 +24,5 @@ class ModuleNumber:
 
             radialposition.append(self.firstposition + i*self.distradialposition)
             
-        return self.nlayers, modules, length,  radialposition
+        return self.nlayers, modules, length,  radialposition, self.phi0_range, self.curvature_range
         
