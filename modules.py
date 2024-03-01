fcfb11b6704ec3bae5f407e81e36d01a5455677c
karakoulaki <k.karakoulaki23@imperial.ac.uk>
2024-03-01 16:45:21 +0000

    changes for new method

--- modules.py
+++ modules.py
@@ -15,9 +15,6 @@ class ModuleNumber:
         self.initiallength=0.63
         self.phi0_range = [np.pi/2,np.pi/2] #np.pi/2 to np.pi/2
 
-        #f[1][0]#[2,38]
-        #f[1][0]#f[1][0]] #using random.normal mean=18 sd=1
-        self.sep=0#0 if uniform, 1 if normal
     def changedetector(self):
         modules=[]
         length=[]
@@ -28,12 +25,12 @@ class ModuleNumber:
         df = pd.read_csv(file_path)
         df = pd.DataFrame(df)
         
-        if self.sep==0:
-            curvature_range.append(df.at[0,'cmin'])
-            curvature_range.append(df.at[0,'cmax'])
-        if self.sep==1:
-            curvature_range.append(df.at[0,'mean'])
-            curvature_range.append(df.at[0,'standard deviation'])
+       
+        #curvature_range.append(df.at[0,'cmin'])
+        #curvature_range.append(df.at[0,'cmax'])
+        
+        curvature_range.append(df.at[0,'mean'])
+        curvature_range.append(df.at[0,'standard deviation'])
        
         for i in range(self.nlayers):
             modules.append(4)
@@ -52,5 +49,5 @@ class ModuleNumber:
         
                 
        
-        return self.nlayers, modules, length,  radialposition, self.phi0_range, curvature_range, self.sep
+        return self.nlayers, modules, length,  radialposition, self.phi0_range, curvature_range
 
