7e833193ead1dd0a1cbadf044fa26af6eea2030b
karakoulaki <k.karakoulaki23@imperial.ac.uk>
2024-05-02 10:56:00 +0100

    new data

--- EncoderMSE.py
+++ EncoderMSE.py
@@ -37,44 +37,33 @@ def parse_string_to_tensor(data_string):
     return torch.tensor(data_list, dtype=torch.float32)
 
 
-df=pd.read_csv("train_data5000.csv")
+df=pd.read_csv("train_datab2.csv")
 df=pd.DataFrame(df) #
-
-ids_to_exclude = [4399120515136, 2199560257568]
-
-# Filter rows to duplicate
-rows_to_duplicate = df[~df['ID'].isin(ids_to_exclude)].copy()
-
-# Concatenate original DataFrame with duplicates
-df = pd.concat([df]  + [rows_to_duplicate]*4, ignore_index=True)
+ 
 
 tensor_list = [parse_string_to_tensor(data_string) for data_string in df['Matrix']]
 X_train_tensors = torch.stack(tensor_list)
 #print(X_train_tensors[0])
 X_train_matrices = X_train_tensors.unsqueeze(1)
 #print(X_train_matrices[0])
-batch_size = 64
+batch_size = 32 # 32
 dataset = CustomDataset(X_train_matrices)
 train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 
-df2=pd.read_csv("test_data.csv")
+df2=pd.read_csv("test_datab.csv")
 df2=pd.DataFrame(df2)
 
 tensor_list2 = [parse_string_to_tensor(data_string) for data_string in df2['Matrix']]
 X_test_tensors = torch.stack(tensor_list2)
 X_test_matrices = X_test_tensors.unsqueeze(1)
 dataset2 = CustomDataset(X_test_matrices)
-test_loader = DataLoader(dataset2, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(dataset2, batch_size=batch_size, shuffle=True)
 
-df3=pd.read_csv("validation_data.csv")
+df3=pd.read_csv("validation_datab.csv")
 df3=pd.DataFrame(df3)
 
-# Filter rows to duplicate
-#rows_to_duplicate3 = df3[~df3['ID'].isin(ids_to_exclude)].copy()
 
-# Concatenate original DataFrame with duplicates
-#df3 = pd.concat([df3]  + [rows_to_duplicate3]*4, ignore_index=True)
 tensor_list3 = [parse_string_to_tensor(data_string) for data_string in df3['Matrix']]
 
 # Stack the tensors to create a batch of input data
@@ -85,6 +74,35 @@ X_test_matrices2 = X_test_tensors2.unsqueeze(1)
 dataset3 = CustomDataset(X_test_matrices2)
 validation_loader = DataLoader(dataset3, batch_size=batch_size, shuffle=True)
 
+
+#testing on specific data
+bound = 20
+curved_data = df2[df2['Curvature'].abs() <= bound]
+straight_data = df2[df2['Curvature'].abs() >= bound]
+
+
+tensor_list4 = [parse_string_to_tensor(data_string) for data_string in curved_data['Matrix']]
+
+# Stack the tensors to create a batch of input data
+curved_tensors = torch.stack(tensor_list4)
+
+# Add channel dimension to the tensors (assuming they represent grayscale images)
+curved_matrices = curved_tensors.unsqueeze(1)
+dataset4 = CustomDataset(curved_matrices)
+curved_loader = DataLoader(dataset4, batch_size=batch_size, shuffle=True)
+
+tensor_list5 = [parse_string_to_tensor(data_string) for data_string in straight_data['Matrix']]
+
+# Stack the tensors to create a batch of input data
+straight_tensors = torch.stack(tensor_list5)
+
+# Add channel dimension to the tensors (assuming they represent grayscale images)
+straight_matrices = straight_tensors.unsqueeze(1)
+dataset5 = CustomDataset(straight_matrices)
+straight_loader = DataLoader(dataset5, batch_size=batch_size, shuffle=True)
+
+
+
 # define the NN architecture
 class ConvAutoencoder(nn.Module):
     def __init__(self):
@@ -92,26 +110,26 @@ class ConvAutoencoder(nn.Module):
         
         # Encoder layers
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 48,3, padding=1),
+            nn.Conv2d(1, 12,3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(48, 4, 3, padding=1),
+            nn.Conv2d(12, 4, 3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
         )
 
         # Decoder layers
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(4, 48, 2, stride=2),
+            nn.ConvTranspose2d(4, 12, 2, stride=2),
             nn.ReLU(),
-            nn.ConvTranspose2d(48, 1, 2, stride=2),
+            nn.ConvTranspose2d(12, 1, 2, stride=2),
             nn.Sigmoid()
         )
 
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
-        return decoded
+        return decoded,encoded
 
 #Training
 model = ConvAutoencoder()
@@ -143,7 +161,7 @@ for epoch in range(1, n_epochs+1):
         # clear the gradients of all optimized variables
         optimizer.zero_grad()
         # forward pass: compute predicted outputs by passing inputs to the model
-        outputs = model(images)
+        outputs = model(images)[0]
         # calculate the loss
         loss = criterion(outputs, images)
        
@@ -174,7 +192,7 @@ for epoch in range(1, n_epochs+1):
     val_loss = 0.0
     for data in validation_loader:
         images = data
-        outputs = model(images)
+        outputs = model(images)[0]
         loss = criterion(outputs, images)  # Reconstruction loss
        
         val_loss += loss.item()*images.size(0)
@@ -195,15 +213,14 @@ plt.show()
 
 #Testing
 
-
-
 test_loss = 0.0
 num_batches = 0
 reconstruction_errors = []
 with torch.no_grad():
     for data in test_loader:
         images=data
-        outputs = model(images)
+        outputs = model(images)[0]
+        outputs2=model(images)[1]
         loss = criterion(outputs, images)
         
         test_loss += loss.item()
@@ -211,11 +228,9 @@ with torch.no_grad():
         reconstruction_errors.append(loss.item()*images.size(0))
         
         num_batches += 1
-        
-        
-        
-        
     
+        
+        
 # Calculate average test loss
 avg_test_loss = test_loss / num_batches
 print("Average Test Loss:", avg_test_loss)
@@ -228,12 +243,16 @@ plt.ylabel('Density')
 plt.title('Histogram of Reconstruction Errors MSE')
 plt.show()
 
+print(images.size())
+print(outputs2.size())
+print(outputs.size())
 
-        
 
 n=19
 plt.figure(figsize=(n+10, 4))
 for i in range(n):
+   # print(images[i])
+    #print(outputs[i])
     plt.subplot(2, n, i + 1)
     plt.imshow(images[i].squeeze().detach().numpy(), cmap='gray')
     plt.title('Original')
@@ -243,3 +262,46 @@ for i in range(n):
     plt.title('Reconstructed')
     plt.axis('off')
 plt.show()
+
+
+#Testing 2
+
+test_loss2 = 0.0
+num_batches2 = 0
+with torch.no_grad():
+    for data in straight_loader:
+        images=data
+        outputs = model(images)[0]
+        outputs2=model(images)[1]
+        loss = criterion(outputs, images)
+        
+        test_loss2 += loss.item()
+        
+        num_batches2 += 1
+    
+        
+
+# Calculate average test loss
+avg_test_loss2 = test_loss2 / num_batches2
+print("Average Test Loss:", avg_test_loss2)
+
+
+
+test_loss3 = 0.0
+num_batches3 = 0
+with torch.no_grad():
+    for data in curved_loader:
+        images=data
+        outputs = model(images)[0]
+        outputs2=model(images)[1]
+        loss = criterion(outputs, images)
+        
+        test_loss3 += loss.item()
+        
+        num_batches3 += 1
+    
+        
+
+# Calculate average test loss
+avg_test_loss3 = test_loss3 / num_batches3
+print("Average Test Loss:", avg_test_loss3)
